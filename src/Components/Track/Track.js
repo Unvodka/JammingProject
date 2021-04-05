2d4c95251e0641545f024fc3c09ce49440c7dcea
arnaud <chevallier.a06@gmail.com>
2021-04-05 12:47:32 +0100

    1 commit

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+        //this.renderAction = this.renderAction.bind(this);
+    }
+
+    renderAction () {
+        if (this.props.isRemoval) {
+            return <button className="Track-action">-</button>
+        } else {
+            return <button className="Track-action">+</button>
+        }
+    }
+
+    addTrack () {
+        this.props.onAdd(this.props.track);
+    }
+
+    render () {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3> {this.props.name} </h3>
+                    <p> {this.props.artist} | {this.props.album} </p>
+                </div>
+                {this.renderAction()}
+            </div>
+        )
+    }
+}
+
+export default Track;
